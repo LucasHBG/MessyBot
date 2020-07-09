d8cc6875252ecf41d6b3f7beca9b5b3922a44fc6
Ant Redstone <32925592+Ant-Redstone@users.noreply.github.com>
2020-07-09 19:27:27 -0300

    v0.01

--- src/commands/forca.js
+++ src/commands/forca.js
@@ -1,9 +1,16 @@
+getWord(){
+	
+}
+
+
 module.exports = {
     name: 'forca',
     description: 'Começa um jogo da forca.',
 	aliases: [''],
-	usage: 'start',
-    execute(message) {
-        
+	//ajeita esse usage ai
+	usage: 'start | chute',
+    execute(message, args) {
+		// qr usar if pros comandos?
+		if(args == 'start')
     },
 };
