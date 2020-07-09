e722ccba121da39878305bba10762b6e8377b50c
Ant Redstone <32925592+Ant-Redstone@users.noreply.github.com>
2020-07-09 19:49:54 -0300

    Update forca.js

--- src/commands/forca.js
+++ src/commands/forca.js
@@ -1,16 +1,30 @@
-getWord(){
-	
+const { dicio } = require('./dicio.json');
+
+function getWord(){
+	return dicio[Math.floor(Math.random() * dicio.length)];
 }
 
 
 module.exports = {
     name: 'forca',
     description: 'Começa um jogo da forca.',
-	aliases: [''],
-	//ajeita esse usage ai
-	usage: 'start | chute',
+	aliases: ['hangman'],
+	//TODO Lukas: ajeita esse usage ai
+	usage: 'start | <chute> | skip | stop | custom',
     execute(message, args) {
-		// qr usar if pros comandos?
-		if(args == 'start')
+		if(args[0] == 'start'){
+			
+		}
     },
 };
+
+/*TODO Geral
+1 - Comando Start Funcional
+1.1 - Leitura Respostas
+1.2 - Validação de Vida
+1.3 - Perder
+2 - Comando Custom Funcional
+3 - Comando de Chute Funcional
+
+
+*/
