7f422d4ec31a4dedf1adbd01a338dfeeaa10ef2a
Lucas <lukahbg@gmail.com>
2020-07-09 22:15:46 -0300

    forca usage v1.0

--- src/commands/forca.js
+++ src/commands/forca.js
@@ -1,24 +1,38 @@
 const { dicio } = require('./dicio.json');
 
-function getWord(){
+function getWord() {
 	return dicio[Math.floor(Math.random() * dicio.length)];
 }
 
-function startGame(msg, word, lifes = 6){
+function startGame(msg, word, lifes = 6) {
 
 }
 
 module.exports = {
-    name: 'forca',
-    description: 'Começa um jogo da forca.',
-	aliases: ['hangman'],
+	name: 'forca',
+	description: 'Começa um jogo da forca.',
+	aliases: ['hangman', 'fc'],
 	//TODO Lukas: ajeita esse usage ai
 	usage: 'start | <chute> | skip | stop | custom',
-    execute(message, args) {
-		if(args[0] == 'start'){
+	execute(message, args) {
+
+		const data = [];
+		const commands = ['start', '<chute>', 'skip', 'stop'];
+
+		if (!args.length) {
+			data.push('Aqui está uma lista com todos os meus comandos em !forca:');
+			data.push('**' + commands.map(command => ' -> !' + command).join(', \n') + '**');
+
+			return message.channel.send(data, { split: true });
+		}
+
+		if (args[1] === 'start') {
 			startGame(message, getWord())
 		}
-    },
+		if (args[1] === 'stop') {
+			message.channel.send('Game over');
+		}
+	},
 };
 
 /*TODO Geral
