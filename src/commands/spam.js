2cfecc0d8a95dde57573c78a9bf3e3d0b05ec5b5
Lucas <lukahbg@gmail.com>
2020-07-11 14:29:23 -0300

    att spam

--- src/commands/spam.js
+++ src/commands/spam.js
@@ -3,11 +3,10 @@ module.exports = {
     description: 'Mencione um membro pra spammar no PV dele.',
     usage: '[numero de mensagens] [url]',
     guildOnly: true,
-    execute(message, args) {
+    async execute(message, args) {
 
-        console.log(args);
         var value = 0;
-        args = (typeof args[1] === 'undefined') ? value = 5 : value = parseInt(args[1]);
+        value = (typeof args[1] === 'undefined') ? 5 : parseInt(args[1]);
 
         if (!message.mentions.users.size) {
             return message.reply('você precisa marcar um usuário antes de spammar ele!');
@@ -16,8 +15,7 @@ module.exports = {
         const taggedUser = message.mentions.users.first();
         var msgSuccecess = true;
 
-        const spamLoop = async _ => {
-            for (let index = 0; index < value; index++) {
+        for (let index = 0; index < value; index++) {
 
                 await taggedUser.send('R-Roi?? 😳😳')
                     .catch(error => {
@@ -25,16 +23,6 @@ module.exports = {
                         msgSuccecess = !msgSuccecess;
                     });
 
-                if (args[2] !== 'undefined')
-                    await taggedUser.send({
-                        files: ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F024%2F207%2Fbrainlettttt.jpg&f=1&nofb=1']
-                    })
-                        .then(console.log)
-                        .catch(error => {
-                            console.error(`Não consegui enviar imagem no PV para ${message.author.tag}.\n`, error);
-                            msgSuccecess = !msgSuccecess;
-                        })
-            }
         }
 
         if (!msgSuccecess)
