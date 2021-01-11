40c4e3bbbe9cdd101b9c818bf8f03c5c8d8f69f4
Lucas <lukahbg@gmail.com>
2021-01-11 16:53:26 -0300

    Ignore unknown commands

--- index.js
+++ index.js
@@ -1,71 +1,79 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // require the discord.js module
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-require('dotenv').config();
-const { DISCORD_TOKEN_MESSY, PREFIX } = process.env
+require("dotenv").config();
+const { DISCORD_TOKEN, PREFIX } = process.env;
 
 // create a new Discord client
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const message = new Discord.Message();
 
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./src/commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const command = require(`./src/commands/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./src/commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
+/** Determines whether or not a message is a user command. */
+function isCommand(message) {
+  return message.content.startsWith(PREFIX);
+}
 
+client.once("ready", () => {
+  console.log("Online!");
 
-client.once('ready', () => {
-    console.log('Online!');
-
-    client.user.setActivity('R-Roi?? 😳😳 [!help]', { type: 'LISTENING' })
-
+  client.user.setActivity("R-Roi?? 😳😳 [!help]", { type: "LISTENING" });
 }); //end client.once ready
 
 //Handle for every messages
-client.on('message', message => {
+client.on("message", (message) => {
+  if (message.author.bot || !isCommand(message)) return;
 
-    if (message.author.bot) return;
+  if (message.author == client.user) return;
 
-    if (message.author == client.user) return;
-    
-    /*
+  /*
         Get the user command "![command]"
-    */
-    let args = message.content.slice(PREFIX.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
+  */
+  let args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-    const command = client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
 
-    if (!command) return;
+  if (!command) return;
 
-    if (command.guildOnly && message.channel.type !== 'text') {
-        return message.reply('Não posso executar esse comando em DMs...');
+  if (command.guildOnly && message.channel.type !== "text") {
+    return message.reply("Não posso executar esse comando em DMs...");
+  }
+
+  if (command.args && !args.length) {
+    let reply = `Você não me passou argumentos, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nO uso correto seria: \`${PREFIX}${command.name} ${command.usage}\``;
     }
 
-    if (command.args && !args.length) {
-        let reply = `Você não me passou argumentos, ${message.author}!`;
+    return message.channel.send(reply);
+  }
 
-        if (command.usage) {
-            reply += `\nO uso correto seria: \`${PREFIX}${command.name} ${command.usage}\``;
-        }
-
-        return message.channel.send(reply);
-    }
-
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error('ERRRRRRRRRO: ' + error);
-        message.channel
-            .send('Não entendi o que você quis dizer. Tente usando `!help [argumento]`. \n\nLista de argumentos: \n   about -> Comenta um pouco sobre o bot. \n   commands -> Mostra a lista de comandos do bot.');
-    }
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error("ERRRRRRRRRO: " + error);
+    message.channel.send(
+      "Não entendi o que você quis dizer. Tente usando `!help [argumento]`. \n\nLista de argumentos: \n   about -> Comenta um pouco sobre o bot. \n   commands -> Mostra a lista de comandos do bot."
+    );
+  }
 });
 
 // login to Discord with your app's token
-client.login(DISCORD_TOKEN_MESSY).catch(err => console.log("" + err));
+client.login(DISCORD_TOKEN).catch((err) => console.log("" + err));
